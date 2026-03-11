afe1715a4b612584aac90a133bfdf9a776fe981f
Abigail Tong <abigail@Abigails-MacBook-Air.local>
2026-03-11 15:59:18 +0800

    Update to JobPilot

---
+++ src/main/java/seedu/JobPilot/Exceptions/JobPilotException.java
@@ -0,0 +1,7 @@
+package seedu.JobPilot.Exceptions;
+
+public class JobPilotException extends Exception{
+    public JobPilotException(String description) {
+        super(description);
+    }
+}
--- src/test/java/seedu/JobPilot/JobPilotTest.java
+++ src/test/java/seedu/JobPilot/JobPilotTest.java
@@ -1,10 +1,10 @@
-package seedu.duke;
+package seedu.JobPilot;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-class DukeTest {
+class JobPilotTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
